6846a5575e55fd39b6d7b0e0b4669bfd2ee3b3eb
mikha-dev <mikha.dev@gmail.com>
2024-01-17 20:14:24 +0300

    home

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
 use Dcat\Admin\Admin;
+use Dcat\Admin\Layout\Content;
+use D4T\Admin\Demos\Http\Controllers\Dashboards\PropSimplifiedDashboard;
 
 Admin::registerLocale();
+
+Admin::home(function(Content $content) {
+        return PropSimplifiedDashboard::make($content);
+} );
